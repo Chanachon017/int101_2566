8451b9b5cdbf64d6839da40259c742ecb1f6513b
Chanachon017 <142304928+Chanachon017@users.noreply.github.com>
2023-10-02 01:02:16 +0700

    Add files via upload

---
+++ int101_hw1_66130500017/src/main/java/work03/MyUtil.java
@@ -0,0 +1,16 @@
+package work03;
+
+
+public class MyUtil {
+    
+    public static double calculateBMI(double weight,double height){
+        double hm;
+        hm = height/100;
+        return weight/(hm*hm);
+    }
+    
+    public static double average(int v1,int v2,int v3){
+        return (v1+v2+v3)/3;
+    }
+
+}
